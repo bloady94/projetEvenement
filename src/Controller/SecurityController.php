074d1c44675f942a8c725d3da1677997b4d99435
bloady94 <alice.quito@gmail.com>
2023-05-30 14:32:51 +0200

    creation main controller, homepage connected, modif security.yaml

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/', name: 'security_login')]
+    #[Route(path: '/', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
@@ -24,7 +24,7 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    #[Route(path: '/logout', name: 'security_logout')]
+    #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
     }
