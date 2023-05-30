ea4e7a03e8bd12b305d157479b3284fb35d32924
lsp <florian.lesp@gmail.com>
2023-05-30 16:49:31 +0200

    Création twig home pour campus + twig ajouter campus.

--- src/Controller/CampusController.php
+++ src/Controller/CampusController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\CampusRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/campus', name: 'app_')]
 class CampusController extends AbstractController
 {
-    #[Route('/campus', name: 'app_campus')]
-    public function index(): Response
+    #[Route('/', name: 'campus')]
+    public function index(CampusRepository $campusRepository): Response
     {
-        return $this->render('campus/index.html.twig', [
+        $campus = $campusRepository->findAll();
+
+        return $this->render('campus/homeCampus.html.twig', [
+            'controller_name' => 'CampusController',
+            'campus' => $campus,
+        ]);
+    }
+
+    #[Route('/add', name: 'addCampus')]
+    public function add(): Response
+    {
+        return $this->render('campus/addCampus.html.twig', [
             'controller_name' => 'CampusController',
         ]);
     }
+
 }
