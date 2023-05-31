db0b53361a8775d0f7cc6dfbdf11a5011c4fda44
bloady94 <alice.quito@gmail.com>
2023-05-31 15:13:04 +0200

    test modifs pages d'erreur

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ErrorController extends AbstractController
+{
+    #[Route('/error', name: 'error')]
+    public function show() : Response
+    {
+        return $this->render('bundles/TwigBundle/Exception/error403.html.twig/');
+    }
+}
