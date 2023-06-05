4446d25636287810ded6d78c0e74f5e9ff24869b
lsp <florian.lesp@gmail.com>
2023-06-05 14:04:34 +0200

    modifications

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,7 @@ use App\Entity\Participant;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -41,8 +42,8 @@ class RegistrationFormType extends AbstractType
             ->add('prenom', TextType::class)
             ->add('telephone', TextType::class)
             ->add('mail', TextType::class)
-            /*->add('administrateur', NumberType::class)
-            ->add('actif', NumberType::class)*/
+            ->add('administrateur', NumberType::class)
+            ->add('actif', NumberType::class)
             ->add('campus', EntityType::class, [
                 'class' => Campus::class,
                 'choice_label' => 'nom'
